71b390a59657bc807f3c0ba72960c4f49e1c6612
Clément Aubin <aubincleme@gmail.com>
2019-07-30 10:42:41 +0200

    #1 : Create a connector for renewing Apache HTTP Basic passwords
    * Add checks in the scripts

--- scripts/rollback_htdigest.sh
+++ scripts/rollback_htdigest.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# exit when any command fails
+set -e
+
 HTDIGEST_PATH=/etc/apache2/htdigest
 HTDIGEST_BACKUP_PATH=/etc/apache2/htdigest.old
 
 # Rollback the file
-mv $HTDIGEST_BACKUP_PATH $HTDIGEST_PATH
+if [ -f $HTDIGEST_BACKUP_PATH ] ; then
+  mv $HTDIGEST_BACKUP_PATH $HTDIGEST_PATH
+else
+  echo 'No backup file found.' >&2
+fi
--- scripts/update_htdigest.sh
+++ scripts/update_htdigest.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# exit when any command fails
+set -e
+
 # Verify that we have enough arguments
 if [ "$#" -ge 3 ] ; then
 
@@ -14,11 +17,14 @@ HTDIGEST_PATH=/etc/apache2/htdigest
 HTDIGEST_BACKUP_PATH=/etc/apache2/htdigest.old
 
 # First, backup the htdigest file
-cp $HTDIGEST_PATH $HTDIGEST_BACKUP_PATH
+if [ -f $HTDIGEST_PATH ] ; then
+  cp $HTDIGEST_PATH $HTDIGEST_BACKUP_PATH
+fi
 
 (echo -n "$HTDIGEST_USER:$HTDIGEST_REALM:" \
- && echo -n "$HTDIGEST_USER:$HTDIGEST_REALM:$HTDIGEST_PASSWORD" | $MD5SUM | $AWK '{print $1}') > htdigest
+ && echo -n "$HTDIGEST_USER:$HTDIGEST_REALM:$HTDIGEST_PASSWORD" \
+ | $MD5SUM | $AWK '{print $1}') > $HTDIGEST_PATH
 else
-  echo "Not enough arguments" >&2
-  echo "Usage : update_htdigest.sh user realm password"
+  echo 'Not enough arguments' >&2
+  echo 'Usage : update_htdigest.sh user realm password'
 fi
